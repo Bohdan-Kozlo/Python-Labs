38c9bc96c6fefcacd0d646feb1c3851fc05842c9
Bohdan-Kozlo <119170585+Bohdan-Kozlo@users.noreply.github.com>
2023-05-25 20:01:04 +0300

    adding the code after submitting the lab

--- models/Chair.py
+++ models/Chair.py
@@ -6,14 +6,25 @@ class Chair(ABC):
     This is abstract chair class
     """
 
-    def __init__(self, id=1, material="wood", max_weight="150", owner="Bob"):
-        self.id = id
+    def __init__(self, reneming=1, material="wood", max_weight="150", owner="Bob"):
+        """
+        This method initializes the fields
+        :param reneming:
+        :param material:
+        :param max_weight:
+        :param owner:
+        """
+        self.reneming = reneming
         self.material = material
         self.max_weight = max_weight
         self.owner = owner
 
     def __str__(self):
-        return f"Id={self.id}, Material={self.material}," \
+        """
+        This method represents the object in the ribbon
+        :return:
+        """
+        return f"Id={self.reneming}, Material={self.material}," \
                f" Max weight={self.max_weight}, Owner={self.owner}"
 
     def occupy(self, owner):
@@ -41,4 +52,3 @@ class Chair(ABC):
         """
         An empty method that is overridden in the class FeedingTable and OfficeChair
         """
-        pass
